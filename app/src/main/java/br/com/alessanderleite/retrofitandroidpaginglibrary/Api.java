97e990752a82c0f25d1d7e7c8a630e7df1c5c08f
alessanderleite <alessander.logistica@gmail.com>
2019-04-19 23:47:48 -0300

    create an interface named Api

---
+++ app/src/main/java/br/com/alessanderleite/retrofitandroidpaginglibrary/Api.java
@@ -0,0 +1,14 @@
+package br.com.alessanderleite.retrofitandroidpaginglibrary;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface Api {
+
+    @GET("answers")
+    Call<StackApiResponse> getAnswers(
+            @Query("page") int page,
+            @Query("pagesize") int pagesize,
+            @Query("site") String site);
+}
