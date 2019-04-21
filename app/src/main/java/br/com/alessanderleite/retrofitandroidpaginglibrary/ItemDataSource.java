d4869c30d6b0aaf695eedf340d6e191fd81ed8b5
alessanderleite <alessander.logistica@gmail.com>
2019-04-20 22:49:42 -0300

    call the three overridden methods loadInitials(), loadBefore() and loadAfter()

--- app/src/main/java/br/com/alessanderleite/retrofitandroidpaginglibrary/ItemDataSource.java
+++ app/src/main/java/br/com/alessanderleite/retrofitandroidpaginglibrary/ItemDataSource.java
@@ -3,6 +3,10 @@ package br.com.alessanderleite.retrofitandroidpaginglibrary;
 import android.arch.paging.PageKeyedDataSource;
 import android.support.annotation.NonNull;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ItemDataSource extends PageKeyedDataSource<Integer, Item> {
 
     //the size of a page that we want
@@ -15,17 +19,62 @@ public class ItemDataSource extends PageKeyedDataSource<Integer, Item> {
     private static final String SITE_NAME = "stackoverflow";
 
     @Override
-    public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull LoadInitialCallback<Integer, Item> callback) {
+    public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull final LoadInitialCallback<Integer, Item> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getAnswers(FIRST_PAGE, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+                        if (response.body() != null) {
+                            callback.onResult(response.body().items, null, FIRST_PAGE + 1);
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
     }
 
     @Override
-    public void loadBefore(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, Item> callback) {
+    public void loadBefore(@NonNull final LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Item> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getAnswers(params.key, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+                        Integer adjacentKey = (params.key > 1) ? params.key - 1 : null;
+                        if (response.body() != null) {
+                            callback.onResult(response.body().items, adjacentKey);
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
     }
 
     @Override
-    public void loadAfter(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, Item> callback) {
+    public void loadAfter(@NonNull final LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Item> callback) {
+        RetrofitClient.getInstance()
+                .getApi()
+                .getAnswers(params.key, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+                        if (response.body() != null) {
+                            Integer key = response.body().has_more ? params.key + 1 : null;
+                            callback.onResult(response.body().items, key);
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
     }
 }
