8a3830aadb93fb3f3b66fe4d0b3c7d49ccf1c1b6
alessanderleite <alessander.logistica@gmail.com>
2019-04-20 23:03:48 -0300

    create the class for ViewModel

---
+++ app/src/main/java/br/com/alessanderleite/retrofitandroidpaginglibrary/ItemViewModel.java
@@ -0,0 +1,25 @@
+package br.com.alessanderleite.retrofitandroidpaginglibrary;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.paging.LivePagedListBuilder;
+import android.arch.paging.PageKeyedDataSource;
+import android.arch.paging.PagedList;
+
+public class ItemViewModel extends ViewModel {
+
+    LiveData<PagedList<Item>> itemPagedList;
+    LiveData<PageKeyedDataSource<Integer, Item>> liveDataSource;
+
+    public ItemViewModel() {
+        ItemDataSourceFactory itemDataSourceFactory = new ItemDataSourceFactory();
+
+        PagedList.Config pagedListConfig =
+                (new PagedList.Config.Builder())
+                .setEnablePlaceholders(false)
+                .setPageSize(ItemDataSource.PAGE_SIZE).build();
+
+        itemPagedList = (new LivePagedListBuilder(itemDataSourceFactory, pagedListConfig))
+                .build();
+    }
+}
